50a8575e378a5bf1cea782e40c2ad9b2992cba6f
izskat <73699431+izskat@users.noreply.github.com>
2022-09-20 20:08:10 +0800

    revert

--- Sinagpala/sinagpala-notebooks-main/[1] Satellite Image Retrieval/Square_Grid_Generator.js
+++ Sinagpala/sinagpala-notebooks-main/[1] Satellite Image Retrieval/Square_Grid_Generator.js
@@ -2,25 +2,15 @@ const { squareGrid, polygon } = require("@turf/turf")
 const { writeFileSync } = require('fs')
 
 // Replace with your json file
-const taguigjson = require('./taguig_polygon.json')['features'][0]['geometry']['coordinates']
-const makatijson = require('./makati_polygon.json')['features'][0]['geometry']['coordinates']
-const laspinasjson = require('./laspinas_polygon.json')['features'][0]['geometry']['coordinates']
+const taguig = require('./taguig_polygon.json')['features'][0]['geometry']['coordinates']
 
 //[minX, minY, maxX, maxY] of your chosen city
-var bboxT = [121.022206,14.451312,121.122151,14.56213];
-var bboxM = [120.998771,14.529634,121.067503,14.579432]
-var bboxL = [120.943353,14.349185,121.024234,14.501115]
+var bbox = [121.02220566, 14.45131156 ,121.12215097, 14.56213046];
 
-function getboxes(json, bbox, city) {
-    var cellSide = 76; //size of each square
-    var poly = polygon(json)
-    var options = {units: 'meters', mask: poly};
-    var sg = squareGrid(bbox, cellSide, options);
-    
-    var dictstring = JSON.stringify(sg);
-    writeFileSync( city + "_boxes.json", dictstring);
-}
+var cellSide = 76; //size of each square
+var poly = polygon(taguig)
+var options = {units: 'meters', mask: poly};
+var sg = squareGrid(bbox, cellSide, options);
 
-getboxes(taguigjson, bboxT, "taguig")
-getboxes(makatijson, bboxM, "makati")
-getboxes(laspinasjson, bboxL, "laspinas")
+var dictstring = JSON.stringify(sg);
+writeFileSync("taguig_boxes.json", dictstring);
